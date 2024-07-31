7508029fccb86ddf61380b3196e6524c14790c0f
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-07-31 18:21:33 +0200

    add correct requirements for building docs and package

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use run_simulation::*;
 /// A Python module implemented in Rust.
 #[pymodule]
 #[pyo3(name="_cr_trichome_rust")]
-fn cr_trichome(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _cr_trichome_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<SimulationSettings>()?;
     m.add_function(wrap_pyfunction!(run_sim, m)?)?;
     Ok(())
