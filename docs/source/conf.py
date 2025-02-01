a46f7b2c00a5df8293c458f22af55890e896d395
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-02-01 21:33:45 +0100

    formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,50 +8,50 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath('../../'))
+
+sys.path.append(os.path.abspath("../../"))
 from python import cr_trichome
 
-project = 'cr_trichome'
-copyright = '2024, Jonas Pleyer, Toquinha Bergmann'
-author = 'Jonas Pleyer, Toquinha Bergmann'
-release = '0.0.1'
+project = "cr_trichome"
+copyright = "2024, Jonas Pleyer, Toquinha Bergmann"
+author = "Jonas Pleyer, Toquinha Bergmann"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
-    'sphinxcontrib.bibtex',
+    "myst_parser",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinxcontrib.bibtex",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
 }
 
 autosummary_generate = True
 add_module_names = False
-autodoc_typehints_format = 'short'
+autodoc_typehints_format = "short"
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 
-bibtex_bibfiles = ['references.bib']
+bibtex_bibfiles = ["references.bib"]
 
 html_theme_options = {
     "show_nav_level": 2,
