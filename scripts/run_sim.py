8169c882e2c64b9b84d74bebbf03600c5e8c7f51
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-02-05 10:48:57 +0100

    actually run something inside script

--- scripts/run_sim.py
+++ scripts/run_sim.py
@@ -3,5 +3,5 @@ import cr_trichome as crt
 if __name__ == "__main__":
     settings = crt.SimulationSettings.default()
 
-    # crt.run_sim(settings)
-    # crt.plot_cells_at_all_iterations(0.0, 4.0, overwrite=True, transparent=True)
+    crt.run_sim(settings)
+    crt.plot_cells_at_all_iterations(0.0, 4.0, overwrite=True, transparent=True)
