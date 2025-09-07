1ab0c51851cdcf75ce194befe9084bb3d996d756
Jonas Pleyer <jonas.dev@pleyer.org>
2025-09-07 02:07:36 +0200

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use run_simulation::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-#[pyo3(name="_cr_trichome_rust")]
+#[pyo3(name = "_cr_trichome_rust")]
 fn _cr_trichome_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<SimulationSettings>()?;
     m.add_function(wrap_pyfunction!(run_sim, m)?)?;
